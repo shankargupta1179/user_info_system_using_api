951206d46c70f7f6ee9f47edff91671a96f08e56
Shankar Gupta <Shankar.Gupta@watchguard.com>
2022-10-13 10:17:04 +0530

    removed comments (earlier code)

--- resources/auth.py
+++ resources/auth.py
@@ -44,25 +44,3 @@ class UserLogin(MethodView):
         
         abort(401,message="Invalid Credentials, please check and try again")
 
-# from flask import Flask,request
-
-# auth = {}
-
-# app = Flask(__name__)
-
-# #this is auth python file 
-# @app.post("/login")
-# def login():
-#     request_data = request.get_json()
-#     if request_data["password"] == auth[request_data["username"]]:
-#         return {"message":"Login Successful"}
-#     return {"message":"Invalid Credentials "}
-
-# @app.post("/signup")
-# def signup():
-#     request_data = request.get_json()
-#     username = request_data["username"]
-#     password = request_data["password"]
-#     new_user = {**request_data,"id":username}
-#     auth[username] = password
-#     return new_user , 201
